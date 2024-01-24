0666b53a922cc9259affc22dad7df4c48d414bc3
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2024-01-25 01:28:44 +0300

    constructor correct

--- src/js/Character.js
+++ src/js/Character.js
@@ -2,30 +2,26 @@ const types = ['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'
 
 export default class Character {
   constructor(name, type) {
-    this.name = this.setName(name);
-    this.type = this.setType(type);
+    this.name = (() => {
+      if (!(typeof (name) === 'string')) {
+        throw new Error('Необходимо передать имя в виде строки');
+      }
+      if (name.length < 2 || name.length > 10) {
+        throw new Error('Длина имени должна быть от 2 до 10 символов');
+      }
+      return name;
+    })();
+    this.type = (() => {
+      if (typeof (type) !== 'string') {
+        throw new Error('Необходимо передать тип в виде строки');
+      }
+      if (!types.includes(type)) {
+        throw new Error('Такого типа не существует');
+      }
+      return type;
+    })();
     this.health = 100;
     this.level = 1;
-  }
-
-  setName(name) {
-    if (!(typeof (name) === 'string')) {
-      throw new Error('Необходимо передать имя в виде строки');
-    }
-    if (name.length < 2 || name.length > 10) {
-      throw new Error('Длина имени должна быть от 2 до 10 символов');
-    }
-    return name;
-  }
-
-  setType(type) {
-    if (typeof (type) !== 'string') {
-      throw new Error('Необходимо передать тип в виде строки');
-    }
-    if (!types.includes(type)) {
-      throw new Error('Такого типа не существует');
-    }
-    return type;
   }
 
   levelUp() {
